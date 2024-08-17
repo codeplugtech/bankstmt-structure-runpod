cb2e7777aa1c047ddb8b79f4d19093b9976d2811
tzktok <tamizh951437@gmail.com>
2024-08-17 15:57:11 +0530

    Update handler.py

--- src/handler.py
+++ src/handler.py
@@ -5,6 +5,7 @@ import os
 import numpy as np
 import cv2
 import runpod
+from runpod.serverless.utils import rp_download
 from pdf2image import convert_from_path
 from main import table_extractor
 
@@ -25,16 +26,17 @@ def handler(job):
 
     user_id = job_input.get('user_id', None)
     doc_id = job_input.get('doc_id', 'output')
-    page_num = 1 if user_id is None else job_input.get('page_num', 1)
+    api = job_input.get('api', None)
+    page_num = int(job_input.get('page_num', 1)) if user_id is not None or api is not None else 1
+    print(page_num)
 
     file_path = job_input.get("file_path")
+    downloaded_file = rp_download.file(file_path)
+    file_path = downloaded_file.get('file_path')
     filename = (os.path.basename(file_path).split('.')[0])
     if file_path.lower().endswith('.pdf'):
-        images = convert_from_path(file_path)
-        if page_num is not None:
-            pages = min(page_num, len(images))
-        else:
-            pages = len(images)
+        images = convert_from_path(file_path, first_page=1, last_page=page_num)
+        pages = min(page_num, len(images)) if page_num is not None else len(images)
     else:
         images = [cv2.imread(file_path)]
         pages = 1
@@ -50,20 +52,26 @@ def handler(job):
         for table_number, crop_image in enumerate(crop_images):
             words = table_extractor.ocr(crop_image)
             structure_result = table_extractor.table_structure(crop_image)
-            table_structures, cells, confidence_score = table_extractor.convert_structure(words, crop_image, structure_result)
+            table_structures, cells, confidence_score = table_extractor.convert_structure(words, crop_image,
+                                                                                          structure_result)
             data_rows = table_extractor.visualize_cells(crop_image, table_structures, cells)
             sheet_name = f'Page_{page_number + 1}_Table_{table_number + 1}'
             df = pd.DataFrame(data_rows)
             df = df.rename(columns=df.iloc[0]).drop(df.index[0])
             df.to_excel(writer, sheet_name=sheet_name, index=False)
             html = table_extractor.cells_to_html(cells)
-    if user_id is None:
-        return {"refresh_worker": False, "job_results": 'html', "html_output": html}
+    if api is None:
+        if user_id is None:
+            return {"refresh_worker": False, "job_results": 'html', "html_output": html}
 
     writer.close()
-    s3_key = f"excel/{doc_id}.xlsx"
-    try:
+    if api == "True":
+        s3_key = f"Api/{doc_id}.xlsx"
+        s3_client.upload_file(output_excel_path, s3_bucket_name, f'{s3_key}')
+    else:
+        s3_key = f"excel/{doc_id}.xlsx"
         s3_client.upload_file(output_excel_path, s3_bucket_name, f'{user_id}/{s3_key}')
+    try:
         print(f"file uploaded successfully")
     except Exception as e:
         return {"error": f"Error uploading to S3: {str(e)}"}
@@ -71,15 +79,5 @@ def handler(job):
     return {"refresh_worker": False, "job_results": {"user_id": f"{user_id}", "doc_id": f"{doc_id}"}}
 
 
-#runpod.serverless.start({"handler": handler})
-if __name__ == "__main__":
-    job = {
-        "input": {
-            "file_path": "ICICI.pdf",
-            "page_num": 3,
-            "user_id": None,
-            "doc_id": 29
-        }
-    }
-    result = handler(job)
-    print(result)
+runpod.serverless.start({"handler": handler})
+
